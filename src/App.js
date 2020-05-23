adeb73673e4acb0838f95d1673d8bacc72effff6
AbhilashRocks <abhilash.tatikonda@focalcxm.com>
2020-05-23 15:55:52 +0530

    IT logger intinal setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+
+function App() {
+  useEffect(() => {
+    M.AutoInit();
+  })
+  return (
+    <div className="App">
+      MY APP
+    </div>
+  );
+}
+
+export default App;
