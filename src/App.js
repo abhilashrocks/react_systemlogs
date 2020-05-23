210b526439b7af0781958033fbb893411554b368
AbhilashRocks <abhilash.tatikonda@focalcxm.com>
2020-05-23 19:57:53 +0530

    UI added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/addTech';
+import TechListModal from './components/techs/TechListModal';
 
 
 function App() {
@@ -9,9 +16,17 @@ function App() {
     M.AutoInit();
   })
   return (
-    <div className="App">
-      MY APP
-    </div>
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <TechListModal />
+        <AddTechModal />
+        <EditLogModal />
+        <AddLogModal />
+        <AddBtn />
+        <Logs />
+      </div>
+    </Fragment>
   );
 }
 
